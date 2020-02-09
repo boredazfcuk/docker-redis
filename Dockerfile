1e629172aee2ce545e97e98f20a6dbc606163b53
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-02-09 23:07:11 +0000

    Should be a change log. If not, I've made so many updates I've forgotten, or too few to bother updating it

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
 FROM alpine:latest
 MAINTAINER boredazfcuk
-ARG app_dependencies="tzdata redis"
+ARG app_dependencies="coreutils tzdata redis"
 ARG data_dir="/var/lib/redis/"
 
 RUN echo "$(date '+%d/%m/%Y - %H:%M:%S') | ***** BUILD STARTED *****" && \
 echo "$(date '+%d/%m/%Y - %H:%M:%S') | Install application dependencies" && \
    apk add --no-cache --no-progress ${app_dependencies}
 
-COPY start-redis.sh /usr/local/bin/start-redis.sh
+COPY entrypoint.sh /usr/local/bin/entrypoint.sh
 COPY healthcheck.sh /usr/local/bin/healthcheck.sh
 
 RUN echo "$(date '+%d/%m/%Y - %H:%M:%S') | Set permissions on launch script" && \
-   chmod +x /usr/local/bin/start-redis.sh /usr/local/bin/healthcheck.sh && \
+   chmod +x /usr/local/bin/entrypoint.sh /usr/local/bin/healthcheck.sh && \
 echo "$(date '+%d/%m/%Y - %H:%M:%S') | ***** BUILD COMPLETE *****"
 
 HEALTHCHECK --start-period=10s --interval=1m --timeout=10s \
@@ -21,4 +21,4 @@ VOLUME "${data_dir}"
 
 WORKDIR "${data_dir}"
 
-CMD "/usr/local/bin/start-redis.sh"
+ENTRYPOINT "/usr/local/bin/entrypoint.sh"
---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/ash
+
+##### Functions #####
+Initialise(){
+   lan_ip="$(hostname -i)"
+   echo -e "\n"
+   echo "$$:E $(date '+%d %b %Y %H:%M:%S.%3N') # ***** Configuring Redis container launch environment *****"
+   echo "$$:E $(date '+%d %b %Y %H:%M:%S.%3N') # Listening IP Address: ${lan_ip}"
+}
+
+Configure(){
+   echo "$$:E $(date '+%d %b %Y %H:%M:%S.%3N') # Bind to listening address"
+   echo "$$:E $(date '+%d %b %Y %H:%M:%S.%3N') # Disable UNIX sockets"
+   echo "$$:E $(date '+%d %b %Y %H:%M:%S.%3N') # Log to /dev/stdout"
+   echo "$$:E $(date '+%d %b %Y %H:%M:%S.%3N') # Set size of tcp backlog queue"
+   sed -i \
+      -e "s/^bind .*$/bind ${lan_ip}/" \
+      -e 's/^\(unixsocket.*\)$/# \1/g' \
+      -e 's#^logfile /var/log/.*#logfile /dev/stdout#' \
+      -e "s#^tcp-backlog .*#tcp-backlog $(cat /proc/sys/net/core/somaxconn)#" \
+      "/etc/redis.conf"
+}
+
+LaunchRedis(){
+   echo "$$:E $(date '+%d %b %Y %H:%M:%S.%3N') # ***** Configuration of Redis container launch environment complete *****"
+   if [ -z "${1}" ]; then
+      echo "$$:E $(date '+%d %b %Y %H:%M:%S.%3N') # Start Redis"
+      exec redis-server /etc/redis.conf
+   else
+      exec "$@"
+   fi
+}
+
+##### Script #####
+Initialise
+Configure
+LaunchRedis
