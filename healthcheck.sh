31d0708a59c1bc4b50cd37ca5fabffe84d15c2a7
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-02-19 01:28:41 +0000

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-if [ "$(nc -z "$(hostname -i)" 6379; echo "${?}")" -ne 0 ]; then
+if [ "$(nc -z "$(hostname -i)" 6379; echo $?)" -ne 0 ]; then
    echo "Redis server not responding on port 6379"
    exit 1
 fi
